c1c012a013956f4f0cb64ddc64ad4938286b452a
tetram <Tetram76@users.noreply.github.com>
2012-10-25 19:18:04 +0000

    Changes from the public version

--- SynEdit/Source/SynEditHighlighterOptions.pas
+++ SynEdit/Source/SynEditHighlighterOptions.pas
@@ -7,17 +7,23 @@ Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
 the specific language governing rights and limitations under the License.
 
-The Original Code is: SynEditHighlighterOptions.pas, released 2012-09-12.
+Datetime format is dd.MM.yyyy hh:mm:ss.
+
+The Original Code is: SynEditHighlighterOptions.pas, released 12.09.2012.
 
 All Rights Reserved.
 
 Contributors to the SynEdit and mwEdit projects are listed in the
 Contributors.txt file.
 
-$Id: SynEditHighlighterOptions.pas,v 1.0 2012/09/12 08:31:23 codehunterworks Exp $
+$Id: SynEditHighlighterOptions.pas,v 1.0.2 25.10.2012 11:16:19 CodehunterWorks Exp $
 
 You may retrieve the latest version of this file at the SynEdit home page,
 located at http://SynEdit.SourceForge.net
+
+Last Changes:
+  21.09.2012 08:37:10 - Moved from String to WideString
+  25.10.2012 11:16:19 - Added DefaultExtension property
 
 Known Issues:
 -------------------------------------------------------------------------------}
@@ -33,22 +39,24 @@ type
   TSynEditHighlighterOptions = class(TPersistent)
   private
     FAutoDetectEnabled: Boolean;
-    FAutoDetectMatchExpression: String;
     FAutoDetectLineLimit: Cardinal;
+    FAutoDetectMatchExpression: WideString;
+    FDefaultExtension: WideString;
+    FLineCommentarEnd: WideString;
+    FLineCommentarStart: WideString;
+    FTitle: WideString;
     FVisible: Boolean;
-    FLineCommentarStart: String;
-    FLineCommentarEnd: String;
-    FTitle: String;
   public
     procedure Assign(Source: TPersistent); override;
     procedure AssignTo(Dest: TPersistent); override;
   published
     property AutoDetectEnabled: Boolean read FAutoDetectEnabled write FAutoDetectEnabled;
     property AutoDetectLineLimit: Cardinal read FAutoDetectLineLimit write FAutoDetectLineLimit;
-    property AutoDetectMatchExpression: String read FAutoDetectMatchExpression write FAutoDetectMatchExpression;
-    property LineCommentarEnd: String read FLineCommentarEnd write FLineCommentarEnd;
-    property LineCommentarStart: String read FLineCommentarStart write FLineCommentarStart;
-    property Title: String read FTitle write FTitle;
+    property AutoDetectMatchExpression: WideString read FAutoDetectMatchExpression write FAutoDetectMatchExpression;
+    property DefaultExtension: WideString read FDefaultExtension write FDefaultExtension;
+    property LineCommentarEnd: WideString read FLineCommentarEnd write FLineCommentarEnd;
+    property LineCommentarStart: WideString read FLineCommentarStart write FLineCommentarStart;
+    property Title: WideString read FTitle write FTitle;
     property Visible: Boolean read FVisible write FVisible;
   end;
 
@@ -61,8 +69,10 @@ begin
       FAutoDetectEnabled:= AutoDetectEnabled;
       FAutoDetectMatchExpression:= AutoDetectMatchExpression;
       FAutoDetectLineLimit:= AutoDetectLineLimit;
+      FDefaultExtension:= DefaultExtension;
       FLineCommentarStart:= LineCommentarStart;
       FLineCommentarEnd:= LineCommentarEnd;
+      FTitle:= Title;
       FVisible:= Visible;
     end;
   end;
@@ -75,8 +85,10 @@ begin
       AutoDetectEnabled:= FAutoDetectEnabled;
       AutoDetectMatchExpression:= FAutoDetectMatchExpression;
       AutoDetectLineLimit:= FAutoDetectLineLimit;
+      DefaultExtension:= FDefaultExtension;
       LineCommentarStart:= FLineCommentarStart;
       LineCommentarEnd:= FLineCommentarEnd;
+      Title:= FTitle;
       Visible:= FVisible;
     end;
   end;
